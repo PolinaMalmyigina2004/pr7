2465576010455c425dc610a494c2044ea59adcc4
PolinaMalmyigina2004 <ellispolly092@gmail.com>
2024-10-16 00:30:20 +0500

    Update homework3.py

--- homework3/homework3.py
+++ homework3/homework3.py
@@ -1,17 +1,18 @@
 def convert_to_base_3(a, d=""):
-    if a > 1:
-        x = a % 3
-        a = a // 3
-        d = str(x) + d
-        return convert_to_base_3(a, d)  # Рекурсивный вызов
-    else:
-        d = str(a) + d  # Добавляем остаток
-        return d
+  if a == 0:
+    return d
+  if a > 0:
+    x = a % 3
+    a = a // 3
+    d = str(x) + d
+    return convert_to_base_3(a, d)
+  else:
+    return "-" + convert_to_base_3(-a, d) 
 
 print("")
 try:
-    a = int(input("Введите число в десятичной системе счисления: "))
-    result = convert_to_base_3(a)
-    print("Число в троичной системе счисления: ", result)
+  a = int(input("Введите число в десятичной системе счисления: "))
+  result = convert_to_base_3(a)
+  print("Число в троичной системе счисления: ", result)
 except ValueError:
-    print('Это не число. Выходим.')
+  print('Это не число. Выходим.')
